7a7c98d6c40ae549e8b0cca79c1f14a4f28b05ec
Karthik Rajan <rajankarthiks@gmail.com>
2014-12-26 11:42:34 +0530

    Env Config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function()
 {
-    return $_ENV['LARAVEL_ENV'] ? $_ENV['LARAVEL_ENV'] : 'local';
+    return $_SERVER['LARAVEL_ENV'] ? $_SERVER['LARAVEL_ENV'] : 'local';
 });
 
 /*
