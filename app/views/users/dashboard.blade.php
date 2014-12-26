4ac2c06972278698c18d0a3db279ea1c99f0a886
Karthik Rajan <rajankarthiks@gmail.com>
2014-12-26 12:11:03 +0530

    View File

--- app/views/users/dashboard.blade.php
+++ app/views/users/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-<h1 class="text-center text-info">Dashboard <small class="text-info">({{ $user->username }})</small></h1>
+<h1 class="text-center text-info">Dashboard <small class="text-info">({{ Auth::user()->username }})</small></h1>
 
 
 @endsection
